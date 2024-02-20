67270dae3c13ac8c775e5c69852cffe415701282
jovicap56 <159878203+jovicap56@users.noreply.github.com>
2024-02-20 14:11:20 +0700

    Add files via upload

---
+++ kasir.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Feb 2024 pada 08.10
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('Jovica', 'jojo');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `detailID` int(11) NOT NULL,
+  `penjualanID` int(11) NOT NULL,
+  `produkID` int(11) NOT NULL,
+  `jumlahProduk` varchar(5) NOT NULL,
+  `subTotal` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_penjualan`
+--
+
+INSERT INTO `detail_penjualan` (`detailID`, `penjualanID`, `produkID`, `jumlahProduk`, `subTotal`) VALUES
+(12, 12, 0, '4', '4'),
+(19, 13, 0, '4', '4'),
+(55566, 1221, 131, '2', '12000'),
+(55567, 1222, 133, '3', '10500'),
+(55568, 1222, 135, '2', '6000'),
+(55569, 1224, 134, '4', '20000'),
+(55570, 1225, 134, '3', '15000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `penjualanID` int(11) NOT NULL,
+  `tanggalPenjualan` date NOT NULL,
+  `totalHarga` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`penjualanID`, `tanggalPenjualan`, `totalHarga`) VALUES
+(1221, '2024-01-13', '12000'),
+(1222, '2024-02-01', '16500'),
+(1224, '2024-02-15', '20000'),
+(1225, '2024-02-20', '15000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `produkID` int(11) NOT NULL,
+  `namaProduk` varchar(30) NOT NULL,
+  `Harga` int(9) NOT NULL,
+  `Stok` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`produkID`, `namaProduk`, `Harga`, `Stok`) VALUES
+(123, 'khisd', 3436, 457),
+(131, 'Minute Maid ', 6000, 10),
+(132, 'ABC Coffee ', 4000, 12),
+(133, 'Teh Pucuk', 3500, 8),
+(134, 'Fruit Tea ', 5000, 5),
+(135, 'Floridina', 3000, 15);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`detailID`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`penjualanID`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`produkID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
