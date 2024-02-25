63c3dcd716243885217e18545e97083a98bf7b49
jovicap56 <159878203+jovicap56@users.noreply.github.com>
2024-02-25 14:08:06 +0700

    Update del.php

--- del.php
+++ del.php
@@ -1,6 +1,21 @@
 <?php
-// include database connection file
 include "koneksi.php";
-$hapus = mysqli_query($koneksi, "delete from detail_penjualan where detailID='$_GET[detailID]'");
-header("location:index.php");
+$ambildata = mysqli_query($koneksi, "select * from detail_penjualan where detailID='$_GET[detailID]'");
+foreach ($ambildata as $k){
+    $id = $k['detailID'];
+    $penjual = $k['penjualanID'];
+    $produk = $k['produkID'];
+    $tgl = $k['tanggalPenjualan'];
+    $jumlah = $k['jumlahProduk'];
+
+        $selSto =mysqli_query($koneksi, "SELECT * FROM produk WHERE produkID='$produk'");
+        $sto    =mysqli_fetch_array($selSto);
+        $stok    =$sto['Stok'];
+        //menghitung sisa stok
+        $sisa    =$stok+$jumlah;
+
+        $upstok= mysqli_query($koneksi, "UPDATE produk SET Stok='$sisa' WHERE produkID='$produk'");
+        $hapus_transaksi = mysqli_query($koneksi, "DELETE from detail_penjualan where detailID='$_GET[detailID]'");
+        header('location:index.php');
+}
  ?>
